e544e4c4d28e71577072eb85c731034877e505cf
Kyaw Htet <kyawhtet@cgm-myanmar.com>
2022-06-07 08:07:24 +0630

    fix table structure

--- doc/veterinary_pharmacy.sql
+++ doc/veterinary_pharmacy.sql
@@ -1,4 +1,4 @@
--- MySQL dump 10.13  Distrib 8.0.28, for macos11 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
 --
 -- Host: localhost    Database: veterinary_pharmacy
 -- ------------------------------------------------------
@@ -24,20 +24,17 @@ DROP TABLE IF EXISTS `cart_details`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `cart_details` (
   `c_id` int NOT NULL AUTO_INCREMENT,
+  `cart_id` int NOT NULL,
+  `medicine_id` int NOT NULL,
   `quantity` int NOT NULL,
   `amount` double NOT NULL,
-  PRIMARY KEY (`c_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  PRIMARY KEY (`c_id`),
+  KEY `FK13f85dcqtvrqvut10srepr43r` (`cart_id`),
+  KEY `FK2lc28wrmw3iac0bl5nexpooxk` (`medicine_id`),
+  CONSTRAINT `FK13f85dcqtvrqvut10srepr43r` FOREIGN KEY (`cart_id`) REFERENCES `shopping_cart` (`id`),
+  CONSTRAINT `FK2lc28wrmw3iac0bl5nexpooxk` FOREIGN KEY (`medicine_id`) REFERENCES `medicines` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=54 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `cart_details`
---
-
-LOCK TABLES `cart_details` WRITE;
-/*!40000 ALTER TABLE `cart_details` DISABLE KEYS */;
-/*!40000 ALTER TABLE `cart_details` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `categories`
@@ -47,25 +44,16 @@ DROP TABLE IF EXISTS `categories`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `categories` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `category_code` varchar(45) DEFAULT NULL,
+  `category_id` int NOT NULL AUTO_INCREMENT,
+  `category_code` varchar(45) NOT NULL,
   `category_name` varchar(255) NOT NULL,
   `created_at` datetime NOT NULL,
   `updated_at` datetime DEFAULT NULL,
   `deleted_at` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`),
+  PRIMARY KEY (`category_id`),
   UNIQUE KEY `category_code_UNIQUE` (`category_code`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `categories`
---
-
-LOCK TABLES `categories` WRITE;
-/*!40000 ALTER TABLE `categories` DISABLE KEYS */;
-/*!40000 ALTER TABLE `categories` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `medicines`
@@ -78,26 +66,24 @@ CREATE TABLE `medicines` (
   `id` int NOT NULL AUTO_INCREMENT,
   `m_name` varchar(100) NOT NULL,
   `m_description` varchar(255) NOT NULL,
-  `m_code` varchar(8) DEFAULT NULL,
+  `m_code` varchar(8) NOT NULL,
   `unit_in_stock` int NOT NULL,
   `amount` double NOT NULL,
-  `image` blob,
+  `image` varchar(255) DEFAULT NULL,
   `created_at` datetime NOT NULL,
   `updated_at` datetime DEFAULT NULL,
   `deleted_at` datetime DEFAULT NULL,
+  `categoryId` int NOT NULL,
+  `category_id` int DEFAULT NULL,
+  `create_user_id` int DEFAULT NULL,
+  `deleted_user_id` int DEFAULT NULL,
+  `photo` varchar(255) DEFAULT NULL,
+  `updated_user_id` int DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `m_code_UNIQUE` (`m_code`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  KEY `FK67kgcomhmvewc8obgigvhvhi` (`categoryId`),
+  CONSTRAINT `FK67kgcomhmvewc8obgigvhvhi` FOREIGN KEY (`categoryId`) REFERENCES `categories` (`category_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=56 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `medicines`
---
-
-LOCK TABLES `medicines` WRITE;
-/*!40000 ALTER TABLE `medicines` DISABLE KEYS */;
-/*!40000 ALTER TABLE `medicines` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `order_details`
@@ -108,21 +94,15 @@ DROP TABLE IF EXISTS `order_details`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `order_details` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `m_name` varchar(45) NOT NULL,
+  `o_id` int NOT NULL,
+  `m_id` int NOT NULL,
   `m_quantity` int NOT NULL,
-  `price` double NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `amount` double NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKrmf6enpsuh70a0r0sry7ixb48` (`m_id`),
+  CONSTRAINT `FKrmf6enpsuh70a0r0sry7ixb48` FOREIGN KEY (`m_id`) REFERENCES `medicines` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=62 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `order_details`
---
-
-LOCK TABLES `order_details` WRITE;
-/*!40000 ALTER TABLE `order_details` DISABLE KEYS */;
-/*!40000 ALTER TABLE `order_details` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `orders`
@@ -136,24 +116,19 @@ CREATE TABLE `orders` (
   `u_name` varchar(45) NOT NULL,
   `u_phone` varchar(20) NOT NULL,
   `u_address` varchar(255) NOT NULL,
-  `m_name` varchar(255) NOT NULL,
-  `m_quantity` int NOT NULL,
+  `date` datetime DEFAULT NULL,
+  `status` tinyint NOT NULL DEFAULT '0',
   `amount` double NOT NULL,
+  `created_user_id` int NOT NULL,
   `created_at` datetime NOT NULL,
-  `updated_at` datetime NOT NULL,
-  `deleted_at` datetime NOT NULL,
+  `updated_user_id` int DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `deleted_user_id` int DEFAULT NULL,
+  `deleted_at` datetime DEFAULT NULL,
+  `orderDetail` tinyblob,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=54 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `orders`
---
-
-LOCK TABLES `orders` WRITE;
-/*!40000 ALTER TABLE `orders` DISABLE KEYS */;
-/*!40000 ALTER TABLE `orders` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `shopping_cart`
@@ -164,23 +139,18 @@ DROP TABLE IF EXISTS `shopping_cart`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `shopping_cart` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `quantity` int NOT NULL,
+  `date` datetime DEFAULT NULL,
+  `checkout_flg` tinyint DEFAULT '0',
   `amount` double NOT NULL,
+  `created_user_id` int NOT NULL,
   `created_at` datetime NOT NULL,
+  `updated_user_id` int DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL,
+  `deleted_user_id` int DEFAULT NULL,
   `deleted_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `shopping_cart`
---
-
-LOCK TABLES `shopping_cart` WRITE;
-/*!40000 ALTER TABLE `shopping_cart` DISABLE KEYS */;
-/*!40000 ALTER TABLE `shopping_cart` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `users`
@@ -193,7 +163,7 @@ CREATE TABLE `users` (
   `id` int NOT NULL AUTO_INCREMENT,
   `username` varchar(50) NOT NULL,
   `email` varchar(45) NOT NULL,
-  `password` varchar(20) NOT NULL,
+  `password` varchar(255) NOT NULL,
   `type` tinyint NOT NULL DEFAULT '1',
   `phone` varchar(20) NOT NULL,
   `address` varchar(255) NOT NULL,
@@ -203,18 +173,14 @@ CREATE TABLE `users` (
   `updated_at` datetime DEFAULT NULL,
   `deleted_user_id` int DEFAULT NULL,
   `deleted_at` datetime DEFAULT NULL,
+  `create_user_id` int DEFAULT NULL,
+  `createdAt` datetime DEFAULT NULL,
+  `deletedAt` datetime DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `updatedAt` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `users`
---
-
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -225,4 +191,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-05-20  8:40:19
+-- Dump completed on 2022-06-07  0:50:27
